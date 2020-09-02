1f4aafd52503eaa467ca5bbfe9c3222e6d0d8e10
Prakasha <prakasha.r@archerongroup.com>
2020-09-02 14:09:07 +0530

    heroku configuration added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,64 +7,4 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'space-launch';
-  spaceResponse: any;
-  jsonResponse: any;
-  launchSuccess: boolean;
-
-  constructor(private http: HttpClient
-    ) { }
-
-    ngOnInit(){
-      
-        this.http
-        .get<any>(`https://api.spacexdata.com/v3/launches?limit=100`)
-        .subscribe(
-          response => {
-          
-            this.spaceResponse = response;
-            this.jsonResponse = response;
-
-          },
-          error => {
-            alert( 'Something went wrong!');
-            console.log( 'Something went wrong!' );                        
-          }
-        );
-    }
-
-    launchSuccessList(success: any){
-
-      this.launchSuccess = success;
-
-      this.spaceResponse =  this.jsonResponse.filter(
-        m =>{
-          return (m.launch_success === success); 
-        } 
-      );
-
-      return this.spaceResponse;
-    }
-
-    launchLandList(success: any){
-
-      this.launchSuccess = true;
-      this.spaceResponse = this.jsonResponse.filter(
-        m =>{
-          return (m.launch_success === success);  
-        } 
-      );
-
-      return this.spaceResponse;
-    }
-
- launchYearsList(year: any){
-
-    this.spaceResponse =  this.jsonResponse.filter(
-      m =>{
-        return (m.launch_year.includes(year)); 
-      } 
-    );
-
-    return this.spaceResponse;
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { HttpClientModule } from '@angular/common/http'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
